3b8c2735907e7f98526869ae50ae8cd17bb44292
bcadolf <adolfbrandon96@gmail.com>
2024-10-06 15:26:15 -0400

    testings js

--- chamber/scripts/main.js
+++ chamber/scripts/main.js
@@ -6,6 +6,29 @@ const today = new Date();
 currentyear.textContent = today.getFullYear();
 lastModified.textContent = `Last Modified: ${document.lastModified}`;
 
-// Set dynamic page title 
-const pageTitle = awaitdocument.getElementsByClassName("page")
+// member card for the directory and other pages
+const directory = document.getElementById('directory');
+const gridType = document.createElement('button');
+const memberData = 'https://bcadolf.github.io/wdd231/chamber/data/members.json';
 
+
+async function getMembersData() {
+    const response = await fetch(memberData);
+    const data = await response.json();
+    console.table(data.members);
+
+    // displayMembers(data.members)
+}
+
+getMembersData()
+
+function createCard(data) {
+    const memberCard = document.createElement('div')
+    memberCard.classList.add = 'card'
+
+    memberCard.innerHTML = ``
+}
+
+const memberList = document.createElement('div')
+
+memberList.classList.add = 'list'
